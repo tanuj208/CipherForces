d452ccc35616b3357ab366e280a1e9ceee532d33
Anant Agarwal <anant@spotnana.com>
2020-10-06 19:49:13 +0530

    Adding helper functions and constants (substitution matrices)

---
+++ encryption_codes/aes/helper.py
@@ -0,0 +1,19 @@
+def text2matrix(text):
+    matrix = []
+    for i in range(16):
+        byte = (text >> (8 * (15 - i))) & 0xFF
+        if i % 4 == 0:
+            matrix.append([byte])
+        else:
+            matrix[i // 4].append(byte)
+    return matrix
+
+
+def matrix2text(matrix):
+    text = 0
+    for i in range(4):
+        for j in range(4):
+            text |= (matrix[i][j] << (120 - 8 * (4 * i + j)))
+    return text
+
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
