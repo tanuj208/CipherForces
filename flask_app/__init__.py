e74ec4a4284683b7ae88d32ca9666b2c021acf7e
gaurav sultane <gaurav.sultane@rubrik.com>
2020-11-17 23:46:45 +0530

    changed database schema

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -10,16 +10,18 @@ class Algorithm(db.Model):
     __tablename__ = "algorithm"
     __table_args__ = {'extend_existing': True} 
     
+    length = 5000
+    s_len = 100
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    type = db.Column(db.String(50))
-    description = db.Column(db.String(100))
-    challenge = db.Column(db.String(100))
-    hint = db.Column(db.String(100))
-    solution = db.Column(db.String(100))
+    name = db.Column(db.String(s_len))
+    type = db.Column(db.String(s_len))
+    description = db.Column(db.String(length))
+    challenge = db.Column(db.String(length))
+    hint = db.Column(db.String(length))
+    solution = db.Column(db.String(length))
     attempts = db.Column(db.Integer)
     success = db.Column(db.Integer)	
-    level = db.Column(db.Integer)
+    level = db.Column(db.String(s_len))
     allow_encrypt = db.Column(db.String(10))
     allow_decrypt = db.Column(db.String(10))
 
